3c93417292b137abc8eb102d5cbaafeb7d349161
Pieter Gleixner <pieter.gleixner@cignium.com>
2019-01-21 12:36:51 -0500

    Initial commit

---
+++ WebDriverDemo/Program.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IWebDriver _driver = new ChromeDriver();
+            _driver.Url = "https://duckduckgo.com/";
+            _driver.Manage().Window.Maximize();
+            Console.WriteLine("Go to " + _driver.Url.ToString());
+
+            Console.WriteLine("Search C#");
+            _driver.FindElement(By.Id("search_form_input_homepage")).SendKeys("c#");
+
+            Console.WriteLine("click button search");
+            _driver.FindElement(By.Id("search_button_homepage")).Click();
+
+            Console.WriteLine("find first link");
+            _driver.FindElement(By.LinkText("https://docs.microsoft.com/en-us/dotnet/csharp/")).Click();
+
+            bool itIsUrl = _driver.Url.Contains("csharp");
+            Screenshot screenshot = ((ITakesScreenshot)_driver).GetScreenshot();
+
+            if (itIsUrl)
+            {
+                screenshot.SaveAsFile("C:\\PitDevelop\\C#\\WebDriver1\\Screenshot\\SearchCompleted.jpg");
+            }
+            else
+            {
+                screenshot.SaveAsFile("C:\\PitDevelop\\C#\\WebDriver1\\Screenshot\\SearchFAIL.jpg");
+            }
+            
+
+
+        }
+    }
+}
